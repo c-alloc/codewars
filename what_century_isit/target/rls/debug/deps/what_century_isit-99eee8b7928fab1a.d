da42841c60bf001040932a6b4cfe991630c28835
reboottext <matheuscordeirobm@gmail.com>
2022-03-22 18:08:01 -0300

    update packages name

---
+++ what_century_isit/target/rls/debug/deps/what_century_isit-99eee8b7928fab1a.d
@@ -0,0 +1,5 @@
+/home/tera/Documents/Projects/codewars-stuffs/what_century_isit/target/rls/debug/deps/what_century_isit-99eee8b7928fab1a.rmeta: src/lib.rs
+
+/home/tera/Documents/Projects/codewars-stuffs/what_century_isit/target/rls/debug/deps/what_century_isit-99eee8b7928fab1a.d: src/lib.rs
+
+src/lib.rs:
---
+++ what_century_isit/target/rls/debug/deps/what_century_isit-9ccc4252db8c8af2.d
@@ -0,0 +1,5 @@
+/home/tera/Documents/Projects/codewars-stuffs/what_century_isit/target/rls/debug/deps/what_century_isit-9ccc4252db8c8af2.rmeta: src/lib.rs
+
+/home/tera/Documents/Projects/codewars-stuffs/what_century_isit/target/rls/debug/deps/what_century_isit-9ccc4252db8c8af2.d: src/lib.rs
+
+src/lib.rs:
